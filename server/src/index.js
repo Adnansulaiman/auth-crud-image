8fb32e8b7fa6e1ca18af035443eca151dadd0f12
Adnan <adnanxyz06@gmail.com>
2025-05-10 20:49:28 +0530

    Configured MongoDB & Added User model

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db.config');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,10 @@ app.get('/',(req,res)=>{
     res.send("hello world")
 })
 
+
+
+// MongoDB connection
+connectDB();
 
 // Create server
 app.listen(PORT,()=>{
